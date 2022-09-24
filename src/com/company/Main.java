d73bf4654f394d99ca93241b394b6abcd9ef6d51
veyss <veysi6188@gmail.com>
2022-09-24 18:21:31 +0300

    soru 41

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Main.asalSayıMı(7);
     }
+
+    public static void asalSayıMı(int sayı) {
+        // asal sayı = sadece kendisi ve 1 e bolunen sayılardır
+        // isAsal True ise sayımız asaldır olsun
+        boolean isAsal = true;
+        for (int i = 2; i < sayı; i++) {
+            if (sayı % i == 0) {
+                isAsal = false;
+                break;
+            }
+        }
+        if(isAsal){
+            System.out.println(sayı + " sayısı Evet asal sayıdır");
+        }
+        else {
+            System.out.println(sayı + " sayısı Hayır asal sayı degildir");
+        }
+
+
+    }
+
 }
